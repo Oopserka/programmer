bf1bcb6779d4ccf763753ab4ed5410d7e49a99df
Oopserka <46574267+Oopserka@users.noreply.github.com>
2021-01-16 23:23:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -26,62 +26,63 @@ int main(void)
   SystemClock_Config();
   MX_GPIO_Init();
 	MX_I2C1_Init();
-  ssd1306_Init (&hi2c1);
+  ssd1306_Init (&hi2c1); // Инициализация дисплея, на котором выводятся принимаемые данные. Используется для отладки
 
 	
-power_and_reset_sequence ();
-program_enable_byte = SPI_Read_Write (program_enable, 4, 3);
+power_and_reset_sequence (); //Функция перезагрузки AVR
+program_enable_byte = SPI_Read_Write (program_enable, 4, 3); // пересылаемый от AVR байт при отправки команды (program enable);
 
 signature[0] = SPI_Read_Write (read_signature1, 4, 4);
 signature[1] = SPI_Read_Write (read_signature2, 4, 4);	
-signature[2] = SPI_Read_Write (read_signature3, 4, 4);
+signature[2] = SPI_Read_Write (read_signature3, 4, 4); //Чтение внутреннего идентификатора
 
 	
-	SPI_Write (chip_erase,4);
+	SPI_Write (chip_erase,4); // Очистка памяти МК
 	
 if (program_enable_byte == 0x53) {
-//Stage1_OK ();
+//Stage1_OK (); //Условная функция проверки. (К примеру, загорается светодиод).
 }
 else 
 {
-	power_and_reset_sequence ();
-//	Stage1_NOK ();
-	reload_counter = reload_counter +1;
+	power_and_reset_sequence (); //Если проверка не пройдена, снова перезагружаем AVR
+//	Stage1_NOK (); // Условная функция ошибки при проверке. ((К примеру, загорается светодиод)
+	reload_counter = reload_counter +1; //Счетчик количества перезагрузок AVR
 }
-if (reload_counter >= 3) NVIC_SystemReset();
+if (reload_counter >= 3) NVIC_SystemReset(); //Если сделали больше 3-х перезагрузок, то перезагружаем сам программатор
 
 if ((signature[0] == 0x1E) && (signature[1] == 0x93) && (signature[1] == 0x1E))
 {
-	// LCD_Print ("Atmega 88PA");
+	// LCD_Print ("Atmega 88PA"); //Условная функция отображения названия микросхемы.
 }
-//else if 
+//else if //В дальнейшем можно добавить друге контроллеры и на экране будет выводиться их название.
 
-for (int u = 0; u <= sizeof (main_program); u++) {
-string_structure[0] = convert (main_program[u][0], main_program[u][1]); // ????????? ????????? ???? ? ??????
-string_structure[1] = convert (main_program[u][2], main_program[u][3]);
-string_structure[2] = convert (main_program[u][4], main_program[u][5]); // 2 ????? ?????????? ?????? ??????
-string_structure[3] = convert (main_program[u][6], main_program[u][7]); // 1 ???? ?????? ? ??????? ??????
+for (int u = 0; u <= sizeof (main_program); u++) { //Начинаем читать строку u-ю HEX-файла
+	//предполагаю, что с СД-карты данные будут идти в виде строк, поэтому сделал функцию для конвертации данных из char[2] в uint8_t
+string_structure[0] = convert (main_program[u][0], main_program[u][1]); // Считываем количество байт данных в строке (стандарт Intel hex);
+string_structure[1] = convert (main_program[u][2], main_program[u][3]); 
+string_structure[2] = convert (main_program[u][4], main_program[u][5]); // считываем 2 байта начального адреса страницы в памяти МК, куда будет все записываться.
+string_structure[3] = convert (main_program[u][6], main_program[u][7]); // Считываем информацию о строке
 	
-SPI_Write ((uint8_t*) 0x4D, 1);
-SPI_Write ((uint8_t*) 0x00, 1);
-SPI_Write ((uint8_t*)string_structure[2], 1);
-SPI_Write ((uint8_t*) 0x00, 1);
-int addr = 0;	
+SPI_Write ((uint8_t*) 0x4D, 1);  //Пишем команду для записи адреса страницы
+SPI_Write ((uint8_t*) 0x00, 1); 
+SPI_Write ((uint8_t*)string_structure[2], 1); //Шлем AVR адрес страницы, на которую будем записывать код
+SPI_Write ((uint8_t*) 0x00, 1); 
+int addr = 0;	//Начальный адрес слова для страницы
 	
 for (o = 4; o <= (string_structure[0] + 4); o++) {
-string_structure[o] = convert (main_program[u][o+3], main_program[u][o+4]); // ??????? ?? ????????? ????? ??????, ??????? ?????????? ?????????
-SPI_Write ((uint8_t*) 0x40, 1);
+string_structure[o] = convert (main_program[u][o+3], main_program[u][o+4]); // В этом цикле мы загоняем все байты данных в массив, из которого потом будем перегонять их в AVR
+SPI_Write ((uint8_t*) 0x40, 1); // команда записи младшего байта 
 SPI_Write ((uint8_t*) 0x00, 1);
-SPI_Write ((uint8_t*) addr , 1);	
-SPI_Write ((uint8_t*) string_structure[o] , 1);	 	
+SPI_Write ((uint8_t*) addr , 1);//адрес слова
+SPI_Write ((uint8_t*) string_structure[o] , 1);	 //передача младшего байта
 
-SPI_Write ((uint8_t*) 0x48, 1);
+SPI_Write ((uint8_t*) 0x48, 1); //команда записи старшего байта
 SPI_Write ((uint8_t*) 0x00, 1);
-SPI_Write ((uint8_t*) addr , 1);	
-SPI_Write ((uint8_t*) string_structure[o+1] , 1);	 	
-	addr = addr +1;
+SPI_Write ((uint8_t*) addr , 1); //адрес слова
+SPI_Write ((uint8_t*) string_structure[o+1] , 1); //передача старшего байта
+	addr = addr +1; //инкременируем адрес слова
 }
-SPI_Write (write_page, 4);
+//На этом я пока и закончил.
 }
 power_and_reset_sequence ();
   while (1)
@@ -91,7 +92,7 @@ power_and_reset_sequence ();
 }
 }
 
-void delay_us(uint32_t us)
+void delay_us(uint32_t us) //функция задержки для SPI
 {
 	uint32_t us_count_tick =  us * (SystemCoreClock/1000000);
 	SCB_DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
@@ -174,7 +175,7 @@ static void MX_I2C1_Init(void)
 
 
 
-int convert (char x, char y) {
+int convert (char x, char y) { //функция для перевода  данных из str в int (да, немного на костылях из-за особенностей кодировки ASCII).
 int out1, out2, out, v,v1;
 	
 out1 = (x - '0');
